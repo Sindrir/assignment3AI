b393eed5b483cc64fa3cf3e0f18ee61e7fcaf8cd
sindreoestrem <sindrost@stud.ntnu.no>
2019-11-18 06:23:51 +0100

    Working prediction with hmm

--- hmm.py
+++ hmm.py
@@ -1,18 +1,31 @@
 import numpy as np
 
-rateOfChange = np.matrix("0.9 0.2; 0.1 0.8")
+theta = np.matrix("0.9 0.2; 0.1 0.8")
 phi = np.matrix("0.1 0.3; 0.2 0.0; 0.4 0.3; 0.0 0.3; 0.3 0.1")
 
-x_0 = np.matrix("0.6; 0.4")
+x_0 = np.matrix("0.6 0.4")
 T = 5
+y = [3, 3, 0, 4, 2]
+#y = [0, 0, 0, 0, 0]
+#y = [4]
 
+def hmm(theta, phi, x_0, y, T):
+    # Hidden Markov Model function
+    # Theta is the transition probability table
+    # Phi is the observation probability table
+    # x_0 is the starting probability of happiness and sadness
+    # y is a list of activities
+    # T is the count of activites/timesteps
+    xT = np.zeros(shape=(6,2))
+    xT[0] = x_0
+    for t in range(T+1):
+        if t != 0:
+            sTMinus = ((phi[y[t-1], 1] * xT[t-1,1]) / ((phi[y[t-1], 1] * xT[t-1,1]) + (phi[y[t-1], 0] * xT[t-1,0])))
+            hTMinus = ((phi[y[t-1], 0] * xT[t-1,0]) / ((phi[y[t-1], 0] * xT[t-1,0]) + (phi[y[t-1], 1] * xT[t-1,1])))
+            top = (phi[y[t-1], 0] * (0.2 * sTMinus + 0.9 * hTMinus))
+            bottom = (phi[y[t-1], 0] * (0.2 * sTMinus + 0.9 * hTMinus)) + (phi[y[t-1], 1] * (1 - (0.2 * sTMinus + 0.9 * hTMinus)))
+            xT[t,0] = top / bottom # probability of happiness
+            xT[t,1] = 1 - xT[t,0]  # probability of sadness
+    return xT
 
-
-x_t = x_0
-for t in range(T):
-    x_t = rateOfChange * x_t
-
-print(x_t)
-py = x_t.T * phi.T
-
-print(py)
+print(hmm(theta, phi, x_0, y, T))
